cbbce54add28f7a3734ffaa21b866e6bafdceb24
richardx14 <richard@dicecentre.org>
2017-11-17 19:31:47 +0000

    added main.tf from vpc

--- vpc/interface.tf
+++ vpc/interface.tf
@@ -12,6 +12,19 @@ variable "enable_dns_hostnames" {
  default = true 
 } 
 variable "enable_dns_support" {
- description = "Should be true if you want to use private DNS within the VPC" default = true 
+ description = "Should be true if you want to use private DNS within the VPC" 
+ default = true 
 }
 
+output "public_subnet_id" {
+ value = "${aws_subnet.public.id}"
+}
+
+output "vpc_id" {
+ value = "${aws_vpc.tfb.id}"
+
+output "cidr" {
+ value = "${aws_vpc.tfb.cidr_block}"
+}
+
+}
