78425705dc8abbfe49e0f004a6ff3297788da96f
richardx14 <richard@dicecentre.org>
2017-11-19 20:11:57 +0000

    removed keys from terraform.tfvars.

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,4 @@
-access_key = "AKIAIPQQWNV7Y7QEGW3A"
-secret_key = "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"
-region = "ami-ecbea388"
+region = "eu-west-2"
 ami = {
  eu-west-2 = "ami-ecbea388"
  us-west-1 = "ami-7c4b331c"
--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  description = "The AWS access key."
-}
-
-variable "secret_key" {
-  description = "The AWS secret key."
-}
-
 variable "region" {
   description = "The AWS region."
   default     = "eu-west-2"
