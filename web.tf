73793989a4b5230aaadc97a983ab5effcfdb0351
richardx14 <richard@dicecentre.org>
2017-11-20 13:50:29 +0000

    changed source of vpc to my github repo

--- web.tf
+++ web.tf
@@ -7,7 +7,7 @@ variable "environment" {
 }
 
 module "vpc" {
-  source        = "./vpc"
+  source        = "github.com/richardx14/tf_vpc.git"
   name          = "web"
   cidr          = "10.0.0.0/16"
   public_subnet = "10.0.1.0/24"
